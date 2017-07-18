245a428912d6661b229176920d3fbbf6b935ddbc
313183373 <313183373@qq.com>
2017-07-18 11:35:58 +0000

    task 4

--- main/student.js
+++ main/student.js
@@ -1,3 +1,13 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+function Student(name,age,klass){
+    Person.call(this);
+    this.name=name;
+    this.age=age;
+    this.klass=klass;
+}
+Student.prototype=new Person();
+Student.prototype.introduce=function(){
+	return 'My name is '+this.name+'. I am '+this.age+' years old. I am a Student. I am at Class '+this.klass+'.';
+};
